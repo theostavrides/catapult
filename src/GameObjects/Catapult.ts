ccd0bc249e114ade62bd61d9f13b73abc1b14d7e
Theo Stavrides <theostavrides@gmail.com>
2023-08-02 14:39:14 -0700

    progress

--- src/GameObjects/Catapult.ts
+++ src/GameObjects/Catapult.ts
@@ -1,8 +1,9 @@
 import { Scene } from "@babylonjs/core/scene";
 import { 
-    SceneLoader, Vector3, MeshBuilder, Mesh, Space, TransformNode, AnimationGroup, ISceneLoaderAsyncResult, setAndStartTimer
+    Vector3, MeshBuilder, Mesh, Space, TransformNode, 
+    AnimationGroup, ISceneLoaderAsyncResult, setAndStartTimer
  } from '@babylonjs/core'
-import InputController from "../InputController";
+import { Level } from "../Levels/TowerLevel";
 
 interface IAnimations {
     fire: AnimationGroup
@@ -10,10 +11,10 @@ interface IAnimations {
 }
 
 export class Catapult {
-    public transformNode: TransformNode
-    public cameraTarget: Mesh
-    private _input: InputController
-    private _scene: Scene
+    level: Level
+    transformNode: TransformNode
+    cameraTarget: Mesh
+    
     private _animations: IAnimations
 
     // Movement Variables
@@ -28,19 +29,31 @@ export class Catapult {
     private _isReloading = false
 
     constructor(
-        scene: Scene, 
-        transformNode: TransformNode, 
-        inputController: InputController,
-        animations: IAnimations
+        level: Level
     ){
-        this.transformNode = transformNode
-        this._scene = scene
-        this._input = inputController
-        this._animations = animations
-        this.cameraTarget = MeshBuilder.CreateBox("cameraTarget", {size: .25 }, scene);
+        this.level = level
+
+        this.transformNode = new TransformNode('cc')
+
+        this.level.assets.catapult.clone('CatapultClone', this.transformNode)
+        this.transformNode.getChildMeshes().forEach(m => m.setEnabled(true))
+        // this.level.scene.
+
+        // Set up follow camera
+        this.cameraTarget = MeshBuilder.CreateBox("cameraTarget", {size: .25 }, this.level.scene)
         this.cameraTarget.position = new Vector3(0, 4.75, 0);
-        this.cameraTarget.parent = transformNode
+        this.cameraTarget.parent = this.transformNode
         this.cameraTarget.visibility = 0
+
+        // Pull animation groups from scene into an easy to use object
+        this._animations = {
+            fire: this.level.scene.animationGroups.find(ag => ag.name === "fire_clean")!,
+            reload: this.level.scene.animationGroups.find(ag => ag.name === "reload_clean")!,
+        }
+
+        this._animations.reload.play()
+        this._animations.reload.goToFrame(this._animations.reload.to)
+        this._animations.reload.stop()
 
         this._registerLoop()        
     } 
@@ -56,7 +69,7 @@ export class Catapult {
 
                 setAndStartTimer({
                     timeout: 1000, 
-                    contextObservable: this._scene.onBeforeRenderObservable,
+                    contextObservable: this.level.scene.onBeforeRenderObservable,
                     onEnded: () => {
                         this._animations.reload.play()
         
@@ -71,27 +84,27 @@ export class Catapult {
     }
 
     private _updateFromControls(){
-        this._deltaTime = this._scene.getEngine().getDeltaTime() / 1000.0;
-        this._rotationY = this._input.rotation / 120 //right, x
-        this._movementZ = this._input.forward / 7 //fwd, z
+        this._deltaTime = this.level.scene.getEngine().getDeltaTime() / 1000.0;
+        this._rotationY = this.level.inputController.rotation / 120 //right, x
+        this._movementZ = this.level.inputController.forward / 7 //fwd, z
         
         // Catapult Translation
-        if (this._input.forwardAxis !== 0) {
+        if (this.level.inputController.forwardAxis !== 0) {
             this.transformNode.locallyTranslate(new Vector3(0,0,this._movementZ))
         }
 
         // Catapult Rotation
-        if (this._input.rotationAxis !== 0) {
-            const rotationDirection = this._input.forwardAxis >= 0 ? 1 : -1
+        if (this.level.inputController.rotationAxis !== 0) {
+            const rotationDirection = this.level.inputController.forwardAxis >= 0 ? 1 : -1
             this.transformNode.rotate(new Vector3(0,1,0), rotationDirection * this._rotationY, Space.WORLD)
         }
 
         // Wheel Rotation
-        if (this._input.forwardAxis === 0) {
-            if (this._input.rotationAxis !== 0) {
+        if (this.level.inputController.forwardAxis === 0) {
+            if (this.level.inputController.rotationAxis !== 0) {
                 // Wheel Rotation when not moving forward or backwards but only turning
                 
-                const rotationAmount = this._input.rotationAxis * .015
+                const rotationAmount = this.level.inputController.rotationAxis * .015
 
                 this.transformNode.getChildMeshes(false, (n) => n.name.startsWith("Wheel.")).filter(n => {
                     if (n.name === "Wheel.fr" || n.name === "Wheel.br") {
@@ -111,7 +124,7 @@ export class Catapult {
 
  
         // Shoot
-        if (this._input.shoot) {
+        if (this.level.inputController.shoot) {
             this._fireCatapult()
         }
 
@@ -119,63 +132,12 @@ export class Catapult {
 
 
     private _registerLoop(){
-        this._scene.registerBeforeRender(() => {
+        this.level.scene.registerBeforeRender(() => {
             this._updateFromControls()
         })
     }
 }
 
-// Reorganize imported animation groups to make them easy to use.
-const reorganizeAnimationGroups = (result: ISceneLoaderAsyncResult, scene: Scene) : IAnimations => {
-    const importedAnimationGroups = result.animationGroups
 
-    const reloadAnimationGroup = new AnimationGroup('reload', scene)
-    const fireAnimationGroup = new AnimationGroup('fire', scene)
-   
-    importedAnimationGroups.forEach(animG => {
-        if (animG.name.startsWith("Reload")){
-            animG.targetedAnimations.forEach(targetedAnimation => {
-                reloadAnimationGroup.addTargetedAnimation(targetedAnimation.animation, targetedAnimation.target)
-           })  
-       }
-        if (animG.name.startsWith("Fire")){
-            animG.targetedAnimations.forEach(targetedAnimation => {
-                fireAnimationGroup.addTargetedAnimation(targetedAnimation.animation, targetedAnimation.target)
-            })  
-        }
 
-        // Get rid of original animation group
-        animG.dispose()
-    })
-
-    // Set catapult in fully ready position so it can fire.
-    reloadAnimationGroup.play(false)
-    reloadAnimationGroup.goToFrame(reloadAnimationGroup.to)
-
-    const animations = {
-        reload: reloadAnimationGroup,
-        fire: fireAnimationGroup,
-    }
-
-    return animations
-}
-
-export const createCatapult = async (scene: Scene, inputController: InputController) => {
-    console.log('wtf?')
-    const result = await SceneLoader.ImportMeshAsync(["Catapult"],'models/', 'catapult.glb', scene)
-    
-    const transformNode = new TransformNode("CatapultTransformNode")
-    const root = result.meshes[0]
-    
-    const catapultModel = root.getChildren().find(c => c.name === "Catapult")
-
-    catapultModel!.parent = transformNode
-
-    root.dispose()
-
-    const animations = reorganizeAnimationGroups(result, scene)
- 
-    return new Catapult(scene, transformNode, inputController, animations)
-}
-
-export default createCatapult
+export default Catapult
--- src/Levels/TowerLevel.ts
+++ src/Levels/TowerLevel.ts
@@ -24,42 +24,48 @@ import "@babylonjs/core/Materials/";
 import { havokModule } from "../externals/havok";
 import { HavokPlugin } from "@babylonjs/core/Physics/v2/Plugins/havokPlugin";
 import { type Game } from "../Game";
-import { type Catapult } from "../GameObjects/Catapult";
+import { Catapult } from "../GameObjects/Catapult";
 import InputController from "../InputController";
 
 export interface Level {
     scene: Scene // The unique scene for the level
+    game: Game
+    assets: IAssets
+    inputController: InputController
 }
 
 interface ITowerLevelParams {
     game: Game
     scene: Scene
+    assets: IAssets
     inputController: InputController
+}
+
+interface IAssets {
+    catapult: TransformNode,
+    rock: TransformNode,
 }
 
 class TowerLevel implements Level {
     scene: Scene
     game: Game
-    private _input: InputController
+    assets: IAssets
+    inputController: InputController
 
-    constructor({ game, scene, inputController } : ITowerLevelParams ){
+    constructor({ game, scene, assets, inputController } : ITowerLevelParams ){
         this.scene = scene
         this.game = game
-        this._input = inputController
+        this.inputController = inputController
+        this.assets = assets
 
-        this._initCamera()
-        this._initLights()        
-        this._initGround()
-        this._initDebugger()
+        this._init()
     }
 
-    private _initCamera(){
-        const camera = new UniversalCamera('fc', new Vector3(10,1,10), this.scene)
-        camera.attachControl(this.game.canvas, true);
-        // const camera = new FollowCamera("FollowCam", new Vector3(0, 4, 0), this.scene, this.catapult.cameraTarget)
-        // camera.rotationOffset = 180
-        // camera.heightOffset = 0
-        // camera.radius = 13
+    private _initCamera(cameraTarget: any){
+        const camera = new FollowCamera("FollowCam", new Vector3(0, 4, 0), this.scene, cameraTarget)
+        camera.rotationOffset = 180
+        camera.heightOffset = 0
+        camera.radius = 13
     }
 
     private _initLights(){
@@ -91,6 +97,16 @@ class TowerLevel implements Level {
             }
         })
     }
+
+    private _init(){
+        this._initLights()        
+        this._initGround()
+        this._initDebugger()
+        
+        const catapult = new Catapult(this)
+
+        this._initCamera(catapult.cameraTarget)
+    }
 }
 
 const createTowerLevel = async ({ game } : { game: Game }) => {
@@ -101,36 +117,32 @@ const createTowerLevel = async ({ game } : { game: Game }) => {
     const inputController = new InputController(scene)
         
     const loadTowerLevelModules = async () => {
-        console.log('i is call')
         return Promise.all([havokModule])
     }
 
-    await Promise.all([
-        importModelsAndAnimations(scene),
+    const [ assets ] = await Promise.all([
+        importAssets(scene),
         loadTowerLevelModules
     ])
         
+
     return new TowerLevel({ 
         game, 
         scene, 
+        assets,
         inputController,
     })
 }
 
-const importModelsAndAnimations = async (scene: Scene) => {
+const importAssets = async (scene: Scene) => {
     // Add models to the existing scene.
     await SceneLoader.AppendAsync('models/','catapult.glb', scene)
 
-    const reorganizeAnimationGroups = () => {    
+    const addCleanAnimationGroupsToScene = () => {  
+        // Create new animation groups in the scene  
         const fireAnimationGroup = new AnimationGroup('fire_clean', scene)
         const reloadAnimationGroup = new AnimationGroup('reload_clean', scene)
        
-        // Disposing of animation groups in the animationGroups.forEach below doesn't work
-        // because dispose() removes the animationGroup from the animationGroups array
-        // that we are currently iterating over.  Instead keep track of the refs in the
-        // toDispose array and dispose after we are done the loop.
-        const toDispose: AnimationGroup[] = []
-
         scene.animationGroups.forEach(ag => {
             if (ag.name === 'Reload' || ag.name.startsWith("Reload.")){
                 ag.targetedAnimations.forEach(targetedAnimation => {
@@ -142,40 +154,48 @@ const importModelsAndAnimations = async (scene: Scene) => {
                     fireAnimationGroup.addTargetedAnimation(targetedAnimation.animation, targetedAnimation.target)
                 })  
             }
-
-            if (!ag.name.endsWith('_clean')) {
-                toDispose.push(ag)
-            }
         })
 
-        toDispose.forEach(ag => ag.dispose())
+        scene.animationGroups
+            .filter(ag => !ag.name.endsWith('_clean'))
+            .forEach(ag => ag.dispose())
     }
 
     const reorganizeModels = () => {
         const root = scene.meshes[0]
 
-        const catapultTransformNode = new TransformNode("CatapultModel", scene)
-        const rockTransformNode = new TransformNode("RockModel", scene)
+        const catapult = new TransformNode("CatapultModel", scene)
+        const rock = new TransformNode("RockModel", scene)
 
         const catapultModel = root.getChildren().find(c => c.name === "Catapult")
-        catapultModel!.parent = catapultTransformNode
+        catapultModel!.parent = catapult
+
 
         const rockModel = root.getChildren().find(c => c.name === "Rock")
-        rockModel!.parent = rockTransformNode
-    
+        rockModel!.parent = rock
+
         // Disables the mesh as we will only use this as a reference to clone
         const disableModel = (node: TransformNode) => {
             node.getChildMeshes().forEach(m => m.setEnabled(false))
         }
 
-        [catapultTransformNode, rockTransformNode].forEach(disableModel)
+        [catapult, rock].forEach(disableModel)
 
         root.dispose()
+
+        return {
+            catapult,
+            rock,
+        }
     }
 
-    reorganizeAnimationGroups()
-    reorganizeModels()
+    addCleanAnimationGroupsToScene()
+    const models = reorganizeModels()
 
+    return models
 }
 
 export default createTowerLevel
+
+
+
